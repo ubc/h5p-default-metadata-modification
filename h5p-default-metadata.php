eeb016320336db881e166c60956186673dae8332
kelvin <kelvin.xu@ubc.ca>
2022-03-02 12:38:23 -0800

    Update plugin version

--- h5p-default-metadata.php
+++ h5p-default-metadata.php
@@ -11,7 +11,7 @@
  * Plugin Name: UBC H5P Addon - Default Metadata Modification
  * Plugin URI:  https://ubc.ca/
  * Description: Modify the default value of h5p content metadata.
- * Version:     1.0.0
+ * Version:     1.0.1
  * Author:      Kelvin Xu
  * Text Domain: ubc-h5p-addon-default-metadata-modification
  * License:     GPL v2 or later
