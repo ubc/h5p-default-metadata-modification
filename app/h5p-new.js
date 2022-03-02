0143323c56878561b7c5b52df9d012fcf078c7a8
kelvin <kelvin.xu@ubc.ca>
2022-03-02 12:29:06 -0800

    Add error handling

--- app/h5p-new.js
+++ app/h5p-new.js
@@ -1,9 +1,17 @@
 (function($){
+    
+    if( ! H5PIntegration || ! H5PIntegration.editor || ! H5PIntegration.editor.metadataSemantics ) {
+        return;
+    }
 
     // Set default license to 'Attributon(CC BY)'
-    H5PIntegration.editor.metadataSemantics[1].default = 'CC BY';
+    if( H5PIntegration.editor.metadataSemantics[1] ) {
+        H5PIntegration.editor.metadataSemantics[1].default = 'CC BY';
+    }
 
     // Set default license version to '4.0 International'
-    H5PIntegration.editor.metadataSemantics[2].default = '4.0';
+    if( H5PIntegration.editor.metadataSemantics[2] ) {
+        H5PIntegration.editor.metadataSemantics[2].default = '4.0';
+    }
 
 })(jQuery); 
--- assets/dist/js/h5p-new.js
+++ assets/dist/js/h5p-new.js
@@ -94,10 +94,19 @@
 /***/ (function(module, exports) {
 
 (function ($) {
-  // Set default license to 'Attributon(CC BY)'
-  H5PIntegration.editor.metadataSemantics[1].default = 'CC BY'; // Set default license version to '4.0 International'
+  if (!H5PIntegration || !H5PIntegration.editor || !H5PIntegration.editor.metadataSemantics) {
+    return;
+  } // Set default license to 'Attributon(CC BY)'
 
-  H5PIntegration.editor.metadataSemantics[2].default = '4.0';
+
+  if (H5PIntegration.editor.metadataSemantics[1]) {
+    H5PIntegration.editor.metadataSemantics[1].default = 'CC BY';
+  } // Set default license version to '4.0 International'
+
+
+  if (H5PIntegration.editor.metadataSemantics[2]) {
+    H5PIntegration.editor.metadataSemantics[2].default = '4.0';
+  }
 })(jQuery);
 
 /***/ })
